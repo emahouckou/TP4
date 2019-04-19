bd2ba7a5379bd652e4150749ffb25d9104084b96
melanie <melanielile02@gmail.com>
2019-04-19 11:18:14 +0200

    Nouveau Projet

---
+++ Entreprise.java
@@ -0,0 +1,13 @@
+
+public class Entreprise {
+	private String nom;
+	private String adresse;
+	private String siege;
+	
+public Entreprise(String nom,String adresse,String siege) {
+	this.nom=nom;
+	this.adresse=adresse;
+	this.siege=siege;
+}
+
+}
---
+++ OffreStage.java
@@ -0,0 +1,17 @@
+
+public class OffreStage {
+	private int id;
+	private String descriptif;
+	private String titre;
+	private String dateDeb;
+	private String duree;
+
+	public OffreStage(int id, String descriptif,String titre,String dateDeb, String duree) {
+		this.id=id;
+		this.descriptif=descriptif;
+		this.titre=titre;
+		this.dateDeb=dateDeb;
+		this.duree=duree;
+		
+	}
+}
